656167a2eb246516bc1e2ba108e9ca889e6f831f
MaluO27 <marialourdesortega_27@hotmail.com>
2023-11-13 18:26:09 -0300

    ORGANIZADO DE ELEMENTOS

--- app.js
+++ app.js
@@ -15,3 +15,21 @@ app.get('/', (req,res) => {
 
 });
 
+app.get('/', (req,res) => {
+    res.sendFile(path.resolve(__dirname,'./views/home.html'));
+
+
+});
+
+app.get('/register', (req,res) => {
+    res.sendFile(path.resolve(__dirname,'./views/register.html'));
+
+
+});
+
+app.get('/login', (req,res) => {
+    res.sendFile(path.resolve(__dirname,'./views/login.html'));
+
+
+});
+
