2f57b0b8618717548d3ff0315d350c5f1845b8a1
MaluO27 <marialourdesortega_27@hotmail.com>
2023-11-13 20:09:36 -0300

    CONFIG: SERVIDOR REMOTO

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const path = require('path');
 const publicPath = path.resolve(__dirname, './public');
 app.use(express.static(publicPath));
 
-app.listen(3000, () => {
+let port= process.env.PORT || 3000;
+app.listen(port, () => {
     console.log('Servidor corriendo en el puerto 3000');
 });
 
