a86c02e673b71006dfec16223b9b75ce64322dbe
MaluO27 <marialourdesortega_27@hotmail.com>
2023-11-13 20:13:40 -0300

    CAMBIOS CONFIG: SERVIDOR REMOTO

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.use(express.static(publicPath));
 
 let port= process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log('Servidor corriendo en el puerto 3000');
+    console.log('Servidor corriendo en el puerto ' + port);
 });
 
 app.get('/', (req,res) => {
